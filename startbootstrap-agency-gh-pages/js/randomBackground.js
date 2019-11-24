cfcc633dbd48d2edcf592e23e3b1287dc6d804d5
RaulOrihuela <raulor97@yahoo.com.mx>
2019-11-24 01:39:10 -0600

    Removed: Removed unused files

--- startbootstrap-agency-gh-pages/js/randomBackground.js
+++ startbootstrap-agency-gh-pages/js/randomBackground.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var urlString = "url(../startbootstrap-agency-gh-pages/img/backgrounds/bg0";
 
-    var index = Math.ceil(((Math.random() * 41) + 1)/10);
+    var index = Math.ceil(((Math.random() * 101) + 1)/20);
     urlString += index.toString() + ".png";
     $('.masthead').css("background-image",urlString);
 });
